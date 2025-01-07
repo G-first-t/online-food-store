eb9cf24d69a5b324c24335c2ca450f8727b1ff11
gregory <gregorykimbira7@gmail.com>
2025-01-07 17:21:54 +0300

    implementing the food service in home component

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FoodService} from "../../../services/food.service";
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit{
+  constructor(private foodService:FoodService ) {
+  }
+ ngOnInit() {
+    return this.foodService.getAllFoods();
+ }
 }
