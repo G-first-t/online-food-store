018528b3375dbc3ddbfcd59ab526f59d2a34cbd0
gregory <gregorykimbira7@gmail.com>
2025-01-07 17:14:15 +0300

    creating food services

--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import {sample_foods} from "../../data";
+import {Food} from "../shared/models/food";
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
 
   constructor() { }
+  getAllFoods():Food[]{
+    return sample_foods;
+  }
+
 }
